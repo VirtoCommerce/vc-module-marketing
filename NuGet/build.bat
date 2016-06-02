6784215b34ee40290aa17d6d68731a7d9895c167
artem-dudarev <ad@virtoway.com>
2016-06-02 11:10:14 +0200

    Added VirtoCommerce.MarketingModule.Client

--- NuGet/build.bat
+++ NuGet/build.bat
@@ -8,6 +8,7 @@ IF NOT DEFINED MSBUILD_PATH SET MSBUILD_PATH=%WINDIR%\Microsoft.NET\Framework\v4
 
 "%MSBUILD_PATH%" "%SOURCE_DIR%\VirtoCommerce.MarketingModule.sln" /nologo /verbosity:m /t:Build /p:Configuration=Release;Platform="Any CPU"
 
+nuget pack "%SOURCE_DIR%\VirtoCommerce.MarketingModule.Client\VirtoCommerce.MarketingModule.Client.csproj" -IncludeReferencedProjects -Symbols -Properties Configuration=Release -o "%TARGET_DIR%"
 nuget pack "%SOURCE_DIR%\VirtoCommerce.MarketingModule.Data\VirtoCommerce.MarketingModule.Data.csproj" -IncludeReferencedProjects -Symbols -Properties Configuration=Release -o "%TARGET_DIR%"
 
 @pause
--- NuGet/publish.bat
+++ NuGet/publish.bat
@@ -1,3 +1,4 @@
-set V=2.10.3
+set V=2.10.4
+nuget push VirtoCommerce.MarketingModule.Client.%V%.nupkg -Source nuget.org -ApiKey %1
 nuget push VirtoCommerce.MarketingModule.Data.%V%.nupkg -Source nuget.org -ApiKey %1
 pause
