390f12ff537cc9a3e380c48732c69ef68b7af2cb
eMazeika <em@virtoway.com>
2016-09-16 14:27:45 +0300

    images: uploader filters removed

--- VirtoCommerce.MarketingModule.Web/Scripts/dynamicContent/blades/placeholders/placeholder-details.js
+++ VirtoCommerce.MarketingModule.Web/Scripts/dynamicContent/blades/placeholders/placeholder-details.js
@@ -16,17 +16,7 @@
                 autoUpload: true,
                 removeAfterUpload: true
             });
-
-            // ADDING FILTERS
-            // Images only
-            uploader.filters.push({
-                name: 'imageFilter',
-                fn: function (i /*{File|FileLikeObject}*/, options) {
-                    var type = '|' + i.type.slice(i.type.lastIndexOf('/') + 1) + '|';
-                    return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
-                }
-            });
-
+            
             uploader.onSuccessItem = function (fileItem, images, status, headers) {
                 blade.entity.imageUrl = images[0].url;
             };
