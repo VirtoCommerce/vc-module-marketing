bf821acfa663e4d3993eebf8002361cf3722bafd
Andrey Kozlov <yecli5@gmail.com>
2019-06-05 16:05:39 +0500

    2.17.6

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Virto Commerce Marketing Module")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2019")]
 
-[assembly: AssemblyFileVersion("2.17.5.0")]
-[assembly: AssemblyVersion("2.17.5.0")]
+[assembly: AssemblyFileVersion("2.17.6.0")]
+[assembly: AssemblyVersion("2.17.6.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
