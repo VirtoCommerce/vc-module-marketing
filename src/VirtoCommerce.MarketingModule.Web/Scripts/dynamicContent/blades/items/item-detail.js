89ffd2ca4a502ed44f0ae901b7e39e562701ab95
Andrei Kolchanov <akak1977@users.noreply.github.com>
2020-12-01 17:43:42 +0500

    VP-6071: Repair save, reset buttons on a content-item detail blade (#120)

--- src/VirtoCommerce.MarketingModule.Web/Scripts/dynamicContent/blades/items/item-detail.js
+++ src/VirtoCommerce.MarketingModule.Web/Scripts/dynamicContent/blades/items/item-detail.js
@@ -30,6 +30,16 @@ angular.module('virtoCommerce.marketingModule')
             }
 
         };
+
+        blade.IsChanged = function () {
+            // Changes check method ensures the fact of different presentation way of dynamic properties:
+            // In the whole object and dynamic properties thru va-generic-value-input template directive
+            var result = angular.equals(_.omit(blade.origEntity, ['dynamicProperties']), _.omit(blade.currentEntity, ['dynamicProperties']));
+            if (blade.origEntity.dynamicProperties && blade.currentEntity.dynamicProperties) {
+                result = result && blade.origEntity.dynamicProperties[0].values[0].valueId == blade.currentEntity.dynamicProperties[0].values[0].value.id;
+            }
+            return !result;
+        };
        
         blade.initialize = function () {
             blade.toolbarCommands = [];
@@ -44,7 +54,7 @@ angular.module('virtoCommerce.marketingModule')
                             blade.saveChanges();
                         },
                         canExecuteMethod: function () {
-                            return !angular.equals(blade.origEntity, blade.currentEntity) && $scope.formScope.$valid;
+                            return blade.IsChanged() && $scope.formScope.$valid;
                         },
                         permission: blade.updatePermission
                     },
@@ -54,7 +64,7 @@ angular.module('virtoCommerce.marketingModule')
                             angular.copy(blade.origEntity, blade.currentEntity);
                         },
                         canExecuteMethod: function () {
-                            return !angular.equals(blade.origEntity, blade.currentEntity);
+                            return blade.IsChanged();
                         },
                         permission: blade.updatePermission
                     },
