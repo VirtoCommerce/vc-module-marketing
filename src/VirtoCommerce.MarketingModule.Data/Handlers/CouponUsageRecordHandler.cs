aad40794ca0fc5b40e4ab63cedec5c6f15d4036c
Egis <em@virtoway.com>
2021-01-19 14:21:59 +0200

    VP-6604: Validate PromotionUsages in CouponUsageRecordHandler (#125)
    * VP-6604: Validate PromotionUsages in CouponUsageRecordHandler
    * VP-6604: Change parameter type (minor refactoring)

--- src/VirtoCommerce.MarketingModule.Data/Handlers/CouponUsageRecordHandler.cs
+++ src/VirtoCommerce.MarketingModule.Data/Handlers/CouponUsageRecordHandler.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Hangfire;
@@ -22,12 +23,14 @@ namespace VirtoCommerce.MarketingModule.Data.Handlers
     {
         private readonly IPromotionUsageService _usageService;
         private readonly IPromotionUsageSearchService _promotionUsageSearchService;
+        private readonly IPromotionService _promotionService;
 
-        public CouponUsageRecordHandler(IPromotionUsageService usageService, IPromotionUsageSearchService promotionUsageSearchService)
+        public CouponUsageRecordHandler(IPromotionUsageService usageService, IPromotionUsageSearchService promotionUsageSearchService, IPromotionService promotionService)
         {
             _usageService = usageService;
             _promotionUsageSearchService = promotionUsageSearchService;
             EqualityComparer = AnonymousComparer.Create((PromotionUsage x) => string.Join(":", x.PromotionId, x.CouponCode, x.ObjectId));
+            _promotionService = promotionService;
         }
 
         private IEqualityComparer<PromotionUsage> EqualityComparer { get; set; }
@@ -45,11 +48,11 @@ namespace VirtoCommerce.MarketingModule.Data.Handlers
 
         [DisableConcurrentExecution(10)]
         // "DisableConcurrentExecutionAttribute" prevents to start simultaneous job payloads.
-	// Should have short timeout, because this attribute implemented by following manner: newly started job falls into "processing" state immediately.
+        // Should have short timeout, because this attribute implemented by following manner: newly started job falls into "processing" state immediately.
         // Then it tries to receive job lock during timeout. If the lock received, the job starts payload.
         // When the job is awaiting desired timeout for lock release, it stucks in "processing" anyway. (Therefore, you should not to set long timeouts (like 24*60*60), this will cause a lot of stucked jobs and performance degradation.)
         // Then, if timeout is over and the lock NOT acquired, the job falls into "scheduled" state (this is default fail-retry scenario).
-	// Failed job goes to "Failed" state (by default) after retries exhausted.
+        // Failed job goes to "Failed" state (by default) after retries exhausted.
         public virtual async Task HandleCouponUsages(OrderChangedEvent message)
         {
             foreach (var changedEntry in message.ChangedEntries)
@@ -65,16 +68,35 @@ namespace VirtoCommerce.MarketingModule.Data.Handlers
 
         private async Task RecordUsages(string objectId, IEnumerable<PromotionUsage> oldUsages, IEnumerable<PromotionUsage> newUsages)
         {
-            var toAddUsages = newUsages.Except(oldUsages, EqualityComparer);
+            var toAddUsages = newUsages.Except(oldUsages, EqualityComparer).ToArray();
             var toRemoveUsages = oldUsages.Except(newUsages, EqualityComparer);
             if (!toAddUsages.IsNullOrEmpty())
             {
-                await _usageService.SaveUsagesAsync(toAddUsages.ToArray());
+                await ValidateUsagesAndThrow(toAddUsages);
+                await _usageService.SaveUsagesAsync(toAddUsages);
             }
             if (!toRemoveUsages.IsNullOrEmpty())
             {
                 var alreadyExistUsages = (await _promotionUsageSearchService.SearchUsagesAsync(new PromotionUsageSearchCriteria { ObjectId = objectId })).Results;
                 await _usageService.DeleteUsagesAsync(alreadyExistUsages.Intersect(toRemoveUsages, EqualityComparer).Select(x => x.Id).ToArray());
+            }
+        }
+
+        private async Task ValidateUsagesAndThrow(IReadOnlyCollection<PromotionUsage> usages)
+        {
+            var invalidUsage = usages.FirstOrDefault(x => x.PromotionId.IsNullOrEmpty());
+            if (invalidUsage != null)
+            {
+                throw new ValidationException($"Invalid PromotionUsage: CouponCode '{invalidUsage.CouponCode}' for objectId '{invalidUsage.ObjectId}' of type {invalidUsage.ObjectType}. PromotionId is missing!");
+            }
+
+            var promotionIds = usages.Select(x => x.PromotionId).ToArray();
+            var promotions = await _promotionService.GetPromotionsByIdsAsync(promotionIds);
+
+            invalidUsage = usages.FirstOrDefault(x => !promotions.Any(p => p.Id == x.PromotionId));
+            if (invalidUsage != null)
+            {
+                throw new ValidationException($"Invalid PromotionUsage: CouponCode '{invalidUsage.CouponCode}' for objectId '{invalidUsage.ObjectId}' of type {invalidUsage.ObjectType}. Promotion with Id '{invalidUsage.PromotionId}' not found!");
             }
         }
 
