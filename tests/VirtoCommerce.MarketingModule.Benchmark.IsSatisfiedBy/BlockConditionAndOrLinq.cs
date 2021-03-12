196719095528d35614426fda582922bbc1f5c3e7
Andrei Kolchanov <akak1977@users.noreply.github.com>
2021-03-12 12:53:48 +0500

    PT-346: Add benchmark test for IsSatisfiedBy: LINQ vs plain code (#135)

---
+++ tests/VirtoCommerce.MarketingModule.Benchmark.IsSatisfiedBy/BlockConditionAndOrLinq.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using VirtoCommerce.CoreModule.Core.Common;
+using VirtoCommerce.Platform.Core.Common;
+
+namespace IsSatisfiedByBench
+{
+    internal class BlockConditionAndOrLinq
+    {
+
+        public string Id { get; protected set; }
+
+        public virtual IList<BlockConditionAndOrLinq> AvailableChildren { get; set; } = new List<BlockConditionAndOrLinq>();
+        public virtual IList<BlockConditionAndOrLinq> Children { get; set; } = new List<BlockConditionAndOrLinq>();
+
+
+        public bool All { get; set; }
+
+        // Logical inverse of expression
+        public bool Not { get; set; } = false;
+
+
+        public bool IsSatisfiedBy(IEvaluationContext context)
+        {
+            var result = false;
+
+            if (Children.IsNullOrEmpty())
+            {
+                return true;
+            }
+
+            if (Children != null && Children.Any())
+            {
+                if (!Not)
+                {
+                    result = All ? Children.All(ch => ch.IsSatisfiedBy(context)) : Children.Any(ch => ch.IsSatisfiedBy(context));
+                }
+                else
+                {
+                    result = All ? !Children.All(ch => ch.IsSatisfiedBy(context)) : !Children.Any(ch => ch.IsSatisfiedBy(context));
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
