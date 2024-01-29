de45589d0c46250b85fbae38ed848b4cbbb7044a
Basil Kotov <basilkot@gmail.com>
2024-01-29 15:12:06 +0000

    use correct permissions to check scopes

--- src/VirtoCommerce.MarketingModule.Web/Controllers/Api/MarketingModulePromotionController.cs
+++ src/VirtoCommerce.MarketingModule.Web/Controllers/Api/MarketingModulePromotionController.cs
@@ -21,7 +21,6 @@ using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.ExportImport;
 using VirtoCommerce.Platform.Core.PushNotifications;
 using VirtoCommerce.Platform.Core.Security;
-using StorePermissions = VirtoCommerce.StoreModule.Core.ModuleConstants.Security.Permissions;
 using webModel = VirtoCommerce.MarketingModule.Web.Model;
 
 namespace VirtoCommerce.MarketingModule.Web.Controllers.Api
@@ -169,7 +168,7 @@ namespace VirtoCommerce.MarketingModule.Web.Controllers.Api
         [Authorize(ModuleConstants.Security.Permissions.Update)]
         public async Task<ActionResult> UpdatePromotions([FromBody] Promotion promotion)
         {
-            var authorizationResult = await _authorizationService.AuthorizeAsync(User, promotion, new MarketingAuthorizationRequirement(StorePermissions.Read, true));
+            var authorizationResult = await _authorizationService.AuthorizeAsync(User, promotion, new MarketingAuthorizationRequirement(ModuleConstants.Security.Permissions.Read, true));
             if (!authorizationResult.Succeeded)
             {
                 return Forbid();
@@ -188,7 +187,7 @@ namespace VirtoCommerce.MarketingModule.Web.Controllers.Api
         public async Task<ActionResult> DeletePromotions([FromQuery] string[] ids)
         {
             var permissionResource = new PermissionResourceModel { PromotionIds = ids };
-            var authorizationResult = await _authorizationService.AuthorizeAsync(User, permissionResource, new MarketingAuthorizationRequirement(StorePermissions.Read, true));
+            var authorizationResult = await _authorizationService.AuthorizeAsync(User, permissionResource, new MarketingAuthorizationRequirement(ModuleConstants.Security.Permissions.Read, true));
             if (!authorizationResult.Succeeded)
             {
                 return Forbid();
@@ -247,7 +246,7 @@ namespace VirtoCommerce.MarketingModule.Web.Controllers.Api
         public async Task<ActionResult> DeleteCoupons([FromQuery] string[] ids)
         {
             var permissionResource = new PermissionResourceModel { CouponIds = ids };
-            var authorizationResult = await _authorizationService.AuthorizeAsync(User, permissionResource, new MarketingAuthorizationRequirement(StorePermissions.Read, true));
+            var authorizationResult = await _authorizationService.AuthorizeAsync(User, permissionResource, new MarketingAuthorizationRequirement(ModuleConstants.Security.Permissions.Read, true));
             if (!authorizationResult.Succeeded)
             {
                 return Forbid();
@@ -264,7 +263,7 @@ namespace VirtoCommerce.MarketingModule.Web.Controllers.Api
         public async Task<ActionResult<ImportNotification>> ImportCouponsAsync([FromBody] ImportRequest request)
         {
             var permissionResource = new PermissionResourceModel { PromotionIds = new[] { request.PromotionId } };
-            var authorizationResult = await _authorizationService.AuthorizeAsync(User, permissionResource, new MarketingAuthorizationRequirement(StorePermissions.Read, true));
+            var authorizationResult = await _authorizationService.AuthorizeAsync(User, permissionResource, new MarketingAuthorizationRequirement(ModuleConstants.Security.Permissions.Read, true));
             if (!authorizationResult.Succeeded)
             {
                 return Forbid();
