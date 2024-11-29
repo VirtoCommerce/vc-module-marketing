b29c7ba5111baf8f64b40a7ce8774661670dca36
Artem Dudarev <artem@virtoworks.com>
2024-11-29 16:56:38 +0200

    Fix formatting

--- src/VirtoCommerce.MarketingModule.Core/Model/Promotions/Rewards/AmountBasedReward.cs
+++ src/VirtoCommerce.MarketingModule.Core/Model/Promotions/Rewards/AmountBasedReward.cs
@@ -4,18 +4,23 @@ namespace VirtoCommerce.MarketingModule.Core.Model.Promotions
 {
     public abstract class AmountBasedReward : PromotionReward
     {
-        protected AmountBasedReward(string rewardType) : base(rewardType)
-        { }
+        protected AmountBasedReward(string rewardType)
+            : base(rewardType)
+        {
+        }
 
         public RewardAmountType AmountType { get; set; }
+
         /// <summary>
         /// Reward amount
         /// </summary>
         public decimal Amount { get; set; }
+
         /// <summary>
         /// The max reward amount limit (Not to exceed $S)
         /// </summary>
         public decimal MaxLimit { get; set; }
+
         /// <summary>
         /// The max  quantity limit (No more than Q items)
         /// </summary>
@@ -44,26 +49,35 @@ namespace VirtoCommerce.MarketingModule.Core.Model.Promotions
             }
 
             var workQuantity = quantity = Math.Max(1, quantity);
+
             if (ForNthQuantity > 0 && InEveryNthQuantity > 0)
             {
                 workQuantity = workQuantity / InEveryNthQuantity * ForNthQuantity;
             }
+
             if (Quantity > 0)
             {
                 workQuantity = Math.Min(Quantity, workQuantity);
             }
+
             var result = Amount * workQuantity;
+
             if (AmountType == RewardAmountType.Relative)
             {
                 result = result * price * 0.01m;
             }
+
             var totalCost = price * quantity;
+
             //use total cost as MaxLimit if it explicitly not set
             var workMaxLimit = MaxLimit > 0 ? MaxLimit : totalCost;
+
             //Do not allow maxLimit be greater that total cost (to prevent reward amount be greater that price)
             workMaxLimit = Math.Min(workMaxLimit, totalCost);
+
             result = Math.Min(workMaxLimit, result);
             result = Math.Min(workMaxLimit, result) / quantity;
+
             return result;
         }
 
