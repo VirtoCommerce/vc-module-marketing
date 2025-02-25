840d48b923daef6feea82907dcaee6a9a8a750d7
Artem Dudarev <artem@virtoworks.com>
2025-02-25 17:26:08 +0200

    Move promotion expression templates from the core module

--- src/VirtoCommerce.MarketingModule.Core/Model/DynamicContent/DynamicContentConditionTreePrototype.cs
+++ src/VirtoCommerce.MarketingModule.Core/Model/DynamicContent/DynamicContentConditionTreePrototype.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using VirtoCommerce.CoreModule.Core.Conditions;
 using VirtoCommerce.CoreModule.Core.Conditions.Browse;
 using VirtoCommerce.CoreModule.Core.Conditions.GeoConditions;
@@ -10,25 +9,28 @@ namespace VirtoCommerce.MarketingModule.Core.Model.DynamicContent
     {
         public DynamicContentConditionTreePrototype()
         {
-            WithAvailConditions(
-              new BlockContentCondition()
-                  .WithAvailConditions(
-                     new ConditionGeoTimeZone(),
-                     new ConditionGeoZipCode(),
-                     new ConditionStoreSearchedPhrase(),
-                     new ConditionAgeIs(),
-                     new ConditionGenderIs(),
-                     new ConditionGeoCity(),
-                     new ConditionGeoCountry(),
-                     new ConditionGeoState(),
-                     new ConditionLanguageIs(),
-                     new UserGroupsContainsCondition(),
-                     new UserGroupIsCondition(),
-                     new DynamicContentConditionCategoryIs(),
-                     new DynamicContentConditionProductIs()
-                   )
-             );
-            Children = AvailableChildren.ToList();
+            IConditionTree[] children =
+            [
+                new BlockContentCondition()
+                    .WithAvailableChildren(
+                        new ConditionGeoTimeZone(),
+                        new ConditionGeoZipCode(),
+                        new ConditionStoreSearchedPhrase(),
+                        new ConditionAgeIs(),
+                        new ConditionGenderIs(),
+                        new ConditionGeoCity(),
+                        new ConditionGeoCountry(),
+                        new ConditionGeoState(),
+                        new ConditionLanguageIs(),
+                        new UserGroupIsCondition(),
+                        new UserGroupsContainsCondition(),
+                        new DynamicContentConditionCategoryIs(),
+                        new DynamicContentConditionProductIs()
+                    ),
+            ];
+
+            WithChildren(children);
+            WithAvailableChildren(children);
         }
     }
 }
