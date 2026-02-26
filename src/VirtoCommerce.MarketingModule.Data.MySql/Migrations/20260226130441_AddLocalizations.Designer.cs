8d1bc441cf070122e8e475706709430d24ab8177
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2026-02-26 15:06:20 +0200

    feat: add Mysql migration

--- src/VirtoCommerce.MarketingModule.Data.MySql/Migrations/20260226130441_AddLocalizations.Designer.cs
+++ src/VirtoCommerce.MarketingModule.Data.MySql/Migrations/20260226130441_AddLocalizations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VirtoCommerce.MarketingModule.Data.Repositories;
 
@@ -11,9 +12,11 @@ using VirtoCommerce.MarketingModule.Data.Repositories;
 namespace VirtoCommerce.MarketingModule.Data.MySql.Migrations
 {
     [DbContext(typeof(MarketingDbContext))]
-    partial class MarketingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260226130441_AddLocalizations")]
+    partial class AddLocalizations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
